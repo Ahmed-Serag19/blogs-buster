9a07482a150dbcf3606b5b5899e0da02ef08a586
Ahmed-Serag19 <ahmedmohamed.amin@hotmail.com>
2024-08-30 12:25:51 +0300

    started about page

--- src/app/about/page.jsx
+++ src/app/about/page.jsx
@@ -1,5 +1,16 @@
+import Image from "next/image";
+
 const AboutPage = () => {
-  return <div>About page</div>;
+  return (
+    <div>
+      <Image
+        src="/about.png"
+        alt="about hero section component"
+        width={500}
+        height={500}
+      />
+    </div>
+  );
 };
 
 export default AboutPage;
