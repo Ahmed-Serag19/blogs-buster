5b47717adbf71c9e7b4a84cf41dda966b436fa2c
Ahmed-Serag19 <ahmedmohamed.amin@hotmail.com>
2024-08-30 11:00:57 +0300

    finished navbar and made a ground for the responsive navbar collapse

--- src/components/navbar/links/Links.jsx
+++ src/components/navbar/links/Links.jsx
@@ -1,32 +1,64 @@
-import Link from "next/link";
+"use client";
 import styles from "./links.module.css";
 import NavLink from "./navLink/navLink";
+import { useState } from "react";
 
+const links = [
+  {
+    title: "Homepage",
+    path: "/",
+  },
+  {
+    title: "About",
+    path: "/about",
+  },
+  {
+    title: "Contact",
+    path: "/contact",
+  },
+  {
+    title: "Blog",
+    path: "/blog",
+  },
+];
 const Links = () => {
-  const links = [
-    {
-      title: "Homepage",
-      path: "/",
-    },
-    {
-      title: "About",
-      path: "/about",
-    },
-    {
-      title: "Contact",
-      path: "/contact",
-    },
-    {
-      title: "Blog",
-      path: "/blog",
-    },
-  ];
+  const [open, setOpen] = useState(false);
+
+  // Temp untill login and auth handling
+  const session = true;
+  const isAdmin = true;
+
   return (
-    <div className={styles.links}>
-      {links.map((link) => (
-        <NavLink item={link} key={link.title} />
-      ))}
-    </div>
+    <>
+      <div className={styles.container}>
+        <div className={styles.links}>
+          {links.map((link) => (
+            <NavLink item={link} key={link.title} />
+          ))}
+          {session ? (
+            <>
+              {isAdmin && <NavLink item={{ title: "Admin", path: "/admin" }} />}
+              <button className={styles.logout}>Logout</button>
+            </>
+          ) : (
+            <NavLink item={{ title: "Login", path: "/login" }} />
+          )}
+        </div>
+        <button
+          className={styles.menuButton}
+          onClick={() => setOpen((prev) => !prev)}
+        >
+          Menu
+        </button>
+        {open && (
+          <div className={styles.mobileLinks}>
+            {links.map((link) => (
+              <NavLink item={link} key={link.title} />
+            ))}
+          </div>
+        )}
+      </div>
+    </>
   );
 };
 
